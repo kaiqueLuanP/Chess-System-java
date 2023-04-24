ad7e7d00c7dfd0499226b7709d9965861a307925
Kaique Luan <zeuslocoxt@gmail.com>
2023-04-24 11:44:52 -0300

    Special move - castling

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -45,8 +45,6 @@ public class Board {
 		piece.position = position;
 	}
 	
-	
-	
 	public Piece removePiece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
